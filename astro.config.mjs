d3e6b554b30fb129ef39bbbb644140e0d50952bf
Daniel Plate <profplate@gmail.com>
2025-05-04 06:35:02 -0500

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://mybitterlifewithjesus.com',
+  site: 'https://buildLittleWorlds.github.io',
+  base: '/bitter-life-with-jesus',
   integrations: [],
 });
